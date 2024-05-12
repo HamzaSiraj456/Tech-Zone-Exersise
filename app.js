0703bece1e63ccb83f58b7847a54c17d8769b046
HamzaSiraj456 <hamzasiraj.770@gmail.com>
2024-05-12 02:20:36 -0700

    My Exersise

---
+++ app.js
@@ -0,0 +1,32 @@
+  
+// Answer no 01
+
+// var age=prompt("Enter your age?");
+
+// if (age >= 18) {
+//     alert("you are an adult")
+// }else{
+//     alert("you are a minor")
+// }
+
+
+//  Answer no 02
+
+//   var userName=prompt("Enter your name" , "Hello stranger!");
+//   alert( "Hallo" + " " + userName + "!")
+
+
+// Answer no 05
+
+
+// var userAge=prompt("Enter your age?");
+
+// if(userAge >= 13  &&  userAge < 19){
+//  alert("you are teenager")
+// }else{
+//     alert("you are not teenager")
+// }
+
+
+
+
